7f84f1ecac6dd4e2a3b0a046e1791a0d6e87d55f
helosantosdesousa <ssheloly@gmail.com>
2025-06-13 11:36:10 -0300

    feature-push notification
    codigo da aula de notificacoes com o firebase

--- lib/tools/notification_handler.dart
+++ lib/tools/notification_handler.dart
@@ -1,6 +1,36 @@
 import 'dart:io';
+import 'package:firebase_messaging/firebase_messaging.dart';
 
+//versao do pdf nao funciona mais 
 class Notifications {
   final void Function(String) callback;
-  FirebaseMess
+  final FirebaseMessaging _firebaseMessaging = FirebaseMessaging.instance;
+
+  Notifications(this.callback);
+
+  void setUpFirebase() async {
+    if (Platform.isIOS) {
+      await _firebaseMessaging.requestPermission(
+        alert: true,
+        badge: true,
+        sound: true,
+      );
+    }
+
+    //token
+    String? token = await _firebaseMessaging.getToken();
+    print("token: $token");
+
+    // mensagens
+    FirebaseMessaging.onMessage.listen((RemoteMessage message) {
+      if (message.notification != null) {
+        callback(message.notification!.body ?? "Mensagem sem corpo");
+      }
+    });
+
+
+    FirebaseMessaging.onMessageOpenedApp.listen((RemoteMessage message) {
+      print("Mensagem clicada: ${message.notification?.body}");
+    });
+  }
 }
