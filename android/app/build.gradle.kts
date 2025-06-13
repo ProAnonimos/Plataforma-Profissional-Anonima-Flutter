3e7aaf9b9893c1ab7ebd5912428214548a28f2f3
helosantosdesousa <ssheloly@gmail.com>
2025-06-13 11:17:49 -0300

    refactor - app name
    mudança de endereço do app pro github da organizacao + pastas melhor organizadas

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 android {
-    namespace = "com.example.plataforma_profissional_anonima"
+    namespace = "com.github.proanonimos.plataforma_profissional_anonima"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
@@ -21,7 +21,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.plataforma_profissional_anonima"
+        applicationId = "com.github.proanonimos.plataforma_profissional_anonima"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
